849cf6ed47fda232e88206820f136301cc77e918
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-08-11 23:16:37 +0300

    Test multiple views per nanoCMS context

--- tests/test_nanocms.py
+++ tests/test_nanocms.py
@@ -1,35 +1,52 @@
 import unittest
 
-from nanocms import DEFAULT_PAGE, navigation, projection, resolve_page
+from nanocms import DEFAULT_PAGE, navigation, projection, resolve_page, resolve_view
 
 
 class NanoCMSTests(unittest.TestCase):
     def test_default_page_resolves(self):
         page = resolve_page(None)
         self.assertEqual(page['id'], DEFAULT_PAGE)
-        self.assertEqual(len(page['placements']), 1)
+        self.assertGreaterEqual(len(page['placements']), 1)
 
     def test_navigation_order_is_stable(self):
         items = navigation()
-        self.assertEqual([item['id'] for item in items], ['canonical-structure', 'raw-json'])
+        self.assertEqual([item['id'] for item in items], ['canonical', 'raw-json'])
 
-    def test_canonical_page_places_canonical_ruleset(self):
-        page = resolve_page('canonical-structure')
-        self.assertEqual(page['placements'][0]['ruleset'], 'CanonicalContract')
-        self.assertEqual(page['placements'][0]['renderer'], 'svg')
+    def test_canonical_context_has_multiple_views(self):
+        page = resolve_page('canonical')
+        self.assertEqual(
+            [p['id'] for p in page['placements']],
+            ['view.canonical_structure_map', 'view.semantic_space_3d'],
+        )
 
-    def test_raw_json_page_places_raw_json_ruleset(self):
+    def test_canonical_views_share_ruleset_and_context_model(self):
+        two_d = resolve_view('canonical', 'view.canonical_structure_map')
+        three_d = resolve_view('canonical', 'view.semantic_space_3d')
+        self.assertEqual(two_d['ruleset'], 'CanonicalContract')
+        self.assertEqual(three_d['ruleset'], 'CanonicalContract')
+        self.assertEqual(two_d['context_model'], 'semantic_identity')
+        self.assertEqual(three_d['context_model'], 'semantic_identity')
+        self.assertEqual(two_d['renderer'], 'svg')
+        self.assertEqual(three_d['renderer'], 'javascript_3d')
+
+    def test_raw_json_context_places_raw_json_view(self):
         page = resolve_page('raw-json')
         self.assertEqual(page['placements'][0]['ruleset'], 'RawJSON')
+        self.assertEqual(page['placements'][0]['context_model'], 'json_pointer')
 
-    def test_projection_exposes_navigation_and_selected_page(self):
-        cms = projection('raw-json')
-        self.assertEqual(cms['selected_page']['id'], 'raw-json')
-        self.assertEqual(len(cms['navigation']), 2)
+    def test_projection_exposes_selected_view_without_changing_page(self):
+        cms = projection('canonical', 'view.semantic_space_3d')
+        self.assertEqual(cms['selected_page']['id'], 'canonical')
+        self.assertEqual(cms['selected_view']['id'], 'view.semantic_space_3d')
 
     def test_unknown_page_fails(self):
         with self.assertRaises(KeyError):
             resolve_page('missing')
+
+    def test_unknown_view_fails(self):
+        with self.assertRaises(KeyError):
+            resolve_view('canonical', 'missing')
 
 
 if __name__ == '__main__':
