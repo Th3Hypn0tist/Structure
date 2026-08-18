02e34301c82e805c13d1a12d38540875dc5fe940
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-08-18 10:46:02 +0300

    Add generic Event item object

---
+++ static/3d/objects/event_item.js
@@ -0,0 +1,15 @@
+// Generic event/list item. The library does not interpret event semantics.
+(() => {
+  const S3D = globalThis.S3D;
+  if (!S3D?.SceneObject) throw new Error('S3D SceneObject must load before event_item');
+
+  class EventItem extends S3D.SceneObject {
+    constructor({ id, label = '', color = [.42, .25, .09], metadata = {} } = {}) {
+      super({ id, metadata });
+      this.label = String(label);
+      this.color = [...color];
+    }
+  }
+
+  S3D.EventItem = EventItem;
+})();
