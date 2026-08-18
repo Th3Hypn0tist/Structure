4fa18ee3c75ad82f52ae9946bd97630d3c8746c4
Aki Hirvilammi <aki.hirvilammi@gmail.com>
2026-08-18 10:45:46 +0300

    Add generic Props item object

---
+++ static/3d/objects/props_item.js
@@ -0,0 +1,16 @@
+// Generic item displayed inside a Props group.
+(() => {
+  const S3D = globalThis.S3D;
+  if (!S3D?.SceneObject) throw new Error('S3D SceneObject must load before props_item');
+
+  class PropsItem extends S3D.SceneObject {
+    constructor({ id, label = '', value = null, color = [.24, .28, .34], metadata = {} } = {}) {
+      super({ id, metadata });
+      this.label = String(label);
+      this.value = value;
+      this.color = [...color];
+    }
+  }
+
+  S3D.PropsItem = PropsItem;
+})();
